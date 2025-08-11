e7275ff0b2b3aa041836126b563b3effb9e26efe
Prakash Kumar <prakashyadav9576@gmail.com>
2025-08-11 10:44:12 +0530

    init admin

---
+++ Admin/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
