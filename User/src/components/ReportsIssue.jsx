7032f6d8ca0f90aaea865e81ccb61e204bd26b3b
ashish7079 <ashishjha25032004@gmail.com>
2025-08-11 14:19:44 +0530

    Update ReportsIssue.jsx

--- User/src/components/ReportsIssue.jsx
+++ User/src/components/ReportsIssue.jsx
@@ -3,11 +3,180 @@ import React from 'react'
 function ReportsIssue() {
   return (
     <div>
-        <div className='border-1 justify-center flex h-150 w-100'>
-        <div className='border-1 justify-center flex h-150 w-100'>
-            report issue
+    <div className='border-1 h-270 w-120 ml-120 mt-10 rounded-md max-sm:hidden max-md:hidden max-lg:hidden'>
+       
+        <div className='mt-7 font-bold text-2xl justify-center flex'>
+            Report Issue
+         </div>
+        <div className='justify-center flex mt-3 text-1xl'>
+           Help improve your city by reporting local problems
         </div>
+            <div className='text-2xl mt-5 ml-5'>
+                Upload photo
+            </div>
+            <div  className='border-1 w-112 h-50 mt-5 ml-3 rounded-xl'>
+
+            </div>
+            <div className='text-2xl mt-5 ml-5'>
+                Your Location
+            </div>
+            <div>
+                <input type="text" placeholder='place your location' className='border-1 rounded-md mt-2 ml-2 w-110 h-10'/>
+            </div>
+              <div className='text-2xl mt-5 ml-5'>
+               landmark
+            </div>
+            <div>
+                <input type="text" placeholder='place your landmark' className='border-1 rounded-md mt-2 ml-2 w-110 h-10'/>
+            </div>
+              <div className='text-2xl mt-5 ml-5'>
+                Issue Title
+            </div>
+            <div>
+                <input type="text" placeholder='place your Issue' className='border-1 rounded-md mt-2 ml-2 w-110 h-10'/>
+            </div>
+                  <div className='text-2xl mt-5 ml-5'>
+                Description(optional)
+            </div>
+            <div>
+                <input type="text" placeholder='place your Description' className='border-1 rounded-md mt-2 ml-2 w-110 h-40'/>
+            </div>
+
+                <div className='flex-row flex gap-5'>
+            <div className='ml-3 mt-5 '> <button className='border-1 rounded-xl w-20 h-7'>Potholes</button> </div>
+                   <div className='ml-3 mt-5 '> <button className='border-1 rounded-xl w-30 h-7'>Street light</button> </div>
+              <div className='ml-3 mt-5 '> <button className='border-1 rounded-xl w-22 h-7'>Electricity</button> </div>
+
+                </div>
+                <div className='flex flex-row gap-5'>
+                 <div className='ml-3 mt-5 '> <button className='border-1 rounded-xl w-21 h-7'>Garbage</button> </div>
+
+            <div className='ml-3 mt-5 '> <button className='border-1 rounded-xl w-20 h-7'>Others</button> </div>
+            </div>
+        
+                  <div className='justify-center flex mt-8 '> <button className='border-1 rounded-2xl w-28 h-10 text-white bg-blue-500 text-2xl'>Submit</button> </div>
+
+
+    </div>
+
+    {/*  */}
+
+ <div className='border-1 h-270 w-120 ml-70 mt-10 rounded-md max-sm:hidden max-md:hidden lg:hidden'>
+       
+        <div className='mt-7 font-bold text-2xl justify-center flex'>
+            Report Issue
+         </div>
+        <div className='justify-center flex mt-3 text-1xl'>
+           Help improve your city by reporting local problems
         </div>
+            <div className='text-2xl mt-5 ml-5'>
+                Upload photo
+            </div>
+            <div  className='border-1 w-112 h-50 mt-5 ml-3 rounded-xl'>
+
+            </div>
+            <div className='text-2xl mt-5 ml-5'>
+                Your Location
+            </div>
+            <div>
+                <input type="text" placeholder='place your location' className='border-1 rounded-md mt-2 ml-2 w-110 h-10'/>
+            </div>
+              <div className='text-2xl mt-5 ml-5'>
+               landmark
+            </div>
+            <div>
+                <input type="text" placeholder='place your landmark' className='border-1 rounded-md mt-2 ml-2 w-110 h-10'/>
+            </div>
+              <div className='text-2xl mt-5 ml-5'>
+                Issue Title
+            </div>
+            <div>
+                <input type="text" placeholder='place your Issue' className='border-1 rounded-md mt-2 ml-2 w-110 h-10'/>
+            </div>
+                  <div className='text-2xl mt-5 ml-5'>
+                Description(optional)
+            </div>
+            <div>
+                <input type="text" placeholder='place your Description' className='border-1 rounded-md mt-2 ml-2 w-110 h-40'/>
+            </div>
+
+                <div className='flex-row flex gap-5'>
+            <div className='ml-3 mt-5 '> <button className='border-1 rounded-xl w-20 h-7'>Potholes</button> </div>
+                   <div className='ml-3 mt-5 '> <button className='border-1 rounded-xl w-30 h-7'>Street light</button> </div>
+              <div className='ml-3 mt-5 '> <button className='border-1 rounded-xl w-22 h-7'>Electricity</button> </div>
+
+                </div>
+                <div className='flex flex-row gap-5'>
+                 <div className='ml-3 mt-5 '> <button className='border-1 rounded-xl w-21 h-7'>Garbage</button> </div>
+
+            <div className='ml-3 mt-5 '> <button className='border-1 rounded-xl w-20 h-7'>Others</button> </div>
+            </div>
+        
+                  <div className='justify-center flex mt-8 '> <button className='border-1 rounded-2xl w-28 h-10 text-white bg-blue-500 text-2xl'>Submit</button> </div>
+
+
+    </div>
+
+   
+    {/*  */}
+
+     <div className='border-1 h-290 w-65 ml-2 mt-10 rounded-md lg:hidden sm:hidden'>
+       
+        <div className='mt-7 font-bold text-2xl justify-center flex'>
+            Report Issue
+         </div>
+        <div className='justify-center flex mt-3 text-1xl'>
+           Help improve your city by reporting local problems
+        </div>
+            <div className='text-2xl mt-5 ml-5'>
+                Upload photo
+            </div>
+            <div  className='border-1 w-50 h-50 mt-5 ml-3 rounded-xl'>
+
+            </div>
+            <div className='text-2xl mt-5 ml-5'>
+                Your Location
+            </div>
+            <div>
+                <input type="text" placeholder='place your location' className='border-1 rounded-md mt-2 ml-2 w-60 h-10'/>
+            </div>
+              <div className='text-2xl mt-5 ml-5'>
+               landmark
+            </div>
+            <div>
+                <input type="text" placeholder='place your landmark' className='border-1 rounded-md mt-2 ml-2 w-60 h-10'/>
+            </div>
+              <div className='text-2xl mt-5 ml-5'>
+                Issue Title
+            </div>
+            <div>
+                <input type="text" placeholder='place your Issue' className='border-1 rounded-md mt-2 ml-2 w-60 h-10'/>
+            </div>
+                  <div className='text-2xl mt-5 
+                  '>
+                Description(optional)
+            </div>
+            <div>
+                <input type="text" placeholder='place your Description' className='border-1 rounded-md mt-2 ml-2 w-60 h-40'/>
+            </div>
+
+                <div className='flex-row flex gap-5'>
+            <div className='ml-3 mt-5 '> <button className='border-1 rounded-xl w-20 h-7'>Potholes</button> </div>
+                   <div className='ml-3 mt-5 '> <button className='border-1 rounded-xl w-30 h-7'>Street light</button> </div>
+
+                </div>
+                <div className='flex flex-row gap-5'>
+                 <div className='ml-3 mt-5 '> <button className='border-1 rounded-xl w-21 h-7'>Garbage</button> </div>
+
+              <div className='ml-3 mt-5 '> <button className='border-1 rounded-xl w-22 h-7'>Electricity</button> </div>
+            </div>
+             <div className='ml-3 mt-5 '> <button className='border-1 rounded-xl w-20 h-7'>Others</button> </div>
+        
+                  <div className='justify-center flex mt-8 '> <button className='border-1 rounded-2xl w-28 h-10 text-white bg-blue-500 text-2xl'>Submit</button> </div>
+
+
+    </div>
+
     </div>
   )
 }
