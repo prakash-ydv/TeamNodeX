a47f5a61f51f3c36d5a46133de3444a647c8c7f6
Golu <golusiddharth88@gmail.com>
2025-08-11 12:30:03 +0530

    add

--- User/src/Home.jsx
+++ User/src/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import FeedCard from './components/FeedPage/FeedCard'
 function Home() {
   return (
     <div>
        <NavBar/>
-      
+      <FeedCard/>
     </div>
   )
 }
